3d34b98b5591cae0113bbcf7e01c0a4daa270514
Hessam Mahdiabadi <67460597+iamHEssam@users.noreply.github.com>
2023-01-09 23:50:46 +0330

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gang
+
+A description of this package.
---
+++ Tests/GangTests/GangTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Gang
+
+final class GangTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Gang().text, "Hello, World!")
+    }
+}
